16bbcbd239ac687280c649a8acc3cf7c09f0add5
Kenneth Chung <ken70r@gmail.com>
2014-10-24 00:23:14 -0700

    switch to ember.prod.js when building for deployment
    Ember prod gets rid of debug statements and assertion calls, so it
    should run a little faster. Unfortunately, there isn't a way to specify
    which ember version to build using browserify because of lack of
    conditionals. The solution here is a bit hacky; we copy either the prod
    or dev version to ember-browserify.js and have browserify serve that
    file as the ember module.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var browserify = require('browserify');
 var buffer     = require('vinyl-buffer');
+var cp         = require('cp');
 var gulp       = require('gulp');
 var source     = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
@@ -7,7 +8,9 @@ var uglify     = require('gulp-uglify');
 var watchify   = require('watchify');
 
 gulp.task('build', function() {
-  return browserify('./public/js/main.js', { debug: true })
+  cp.sync('bower_components/ember/ember.prod.js', 'bower_components/ember/ember-browserify.js');
+
+  return browserify('./public/js/main.js')
     .bundle()
     .pipe(source('app.js'))
     .pipe(buffer())
@@ -18,8 +21,9 @@ gulp.task('build', function() {
 });
 
 gulp.task('watch', function() {
-  watchify.args.debug = true;
+  cp.sync('bower_components/ember/ember.js', 'bower_components/ember/ember-browserify.js');
 
+  watchify.args.debug = true;
   var bundler = watchify(browserify('./public/js/main.js', watchify.args));
 
   bundler.on('update', rebundle);
